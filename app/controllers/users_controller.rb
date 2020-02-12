fc26290b43b149afd367383e81dadc02426c4b1f
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-12 13:37:02 +0900

    Generate controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def following
+  end
+
+  def followers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/following'
+
+  get 'users/followers'
+
+  get 'static_pages/home'
+
+  get 'sessions/new'
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'password_resets_controller/new'
+
+  get 'password_resets_controller/edit'
+
+  get 'dreams/show'
+
+  get 'likes/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
