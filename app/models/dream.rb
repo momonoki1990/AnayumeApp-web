5e75dd776367655f1101dccb5d53f8791cf15a1c
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-15 08:43:34 +0900

    Modify user.rb

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Dream < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  
+
   validates :user_id, presence: true
   validates :content, presence: true
-
 end
