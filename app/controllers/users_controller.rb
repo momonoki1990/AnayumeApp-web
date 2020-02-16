493b919b7b2c03b55eac6c924dde2f394ae6bc0c
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-17 06:48:36 +0900

    Make user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show; end
+  def show
+    @user = User.find_by(params[:id])
+  end
 
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録に成功しました！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
   def edit; end
 
   def following; end
 
   def followers; end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                      :password_confirmation)
+    end
 end
