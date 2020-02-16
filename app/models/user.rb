ec8fce5b89c78c41816db5972774f85644a170c9
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-16 14:58:01 +0900

    Make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :dreams, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
@@ -17,4 +18,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
