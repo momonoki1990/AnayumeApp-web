f0e7f9ad7997c3f4963f94bdce47c62428b8a245
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-18 06:07:05 +0900

    Implement advaced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "b" * 6
     assert @user.valid?
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?("")
+  end
 end
