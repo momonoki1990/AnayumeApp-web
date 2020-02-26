c0abcee2b6fdd30b2e75be6d36e7df5a2190c523
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-02-26 17:33:26 +0900

    Fix error

--- db/migrate/20200214113224_create_dreams.rb
+++ db/migrate/20200214113224_create_dreams.rb
@@ -1,11 +1,11 @@
-class CreateDreams < ActiveRecord::Migration[5.1]
+class CreateDreamposts < ActiveRecord::Migration[5.1]
   def change
-    create_table :dreams do |t|
+    create_table :dreamposts do |t|
       t.text :content
       t.references :user, foreign_key: true
 
       t.timestamps
     end
-    add_index :dreams, [:user_id, :created_at]
+    add_index :dreamposts, [:user_id, :created_at]
   end
 end
