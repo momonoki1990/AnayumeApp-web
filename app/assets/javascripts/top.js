b87f86c602c567e9e48b539064f951857688d7c0
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-03-26 12:24:14 +0900

    [fix]フロントの改善

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,15 +1,17 @@
-$(function() {
-	setTimeout(function(){
+$(function () {
+	setTimeout(function () {
 		$('.start p').fadeIn(800);
-	},200);
-	setTimeout(function(){
+	}, 200);
+	setTimeout(function () {
 		$('.start').fadeOut(400);
-	}, 1500);
-	
+	}, 2000);
+
 	//初回アクセス時(セッション毎)のみ表示
-    if(sessionStorage.getItem('key')){
-        $('#first').css({display:'none'});
-    }
-    add_access = $('body').addClass('access')
-    window.sessionStorage.setItem('key', add_access);
+	if (sessionStorage.getItem('key')) {
+		$('#first').css({
+			display: 'none'
+		});
+	}
+	add_access = $('body').addClass('access')
+	window.sessionStorage.setItem('key', add_access);
 });
