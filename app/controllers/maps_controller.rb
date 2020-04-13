bde16cd640b907dc3761cff031a30b5ffb29e425
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-04-13 09:42:46 +0900

    [fix]地図機能に関するテスト追加

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,7 @@
 class MapsController < ApplicationController
   before_action :logged_in_user, only: %i[index create destroy]
   before_action :correct_user, only: :destroy
-  
+
   def index
     @map = Map.new
     @maps = current_user.map_list
--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -1,5 +1,37 @@
 require 'test_helper'
 
 class MapsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:naoya)
+    @other_user = users(:tarou)
+  end
 
+  test "should redirect index when not logged in" do
+    get maps_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Map.count' do
+      post maps_path, params: { map: { address: "東京", latitude: "35.681", longitude: "139.767", title: "東京" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect index when logged in as wrong user" do
+    log_in_as(@other_user)
+    get maps_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect destroy for wrong map" do
+    log_in_as(@other)
+    map = maps(:egypt)
+    assert_no_difference 'Map.count' do
+      delete map_path(map)
+    end
+    assert_redirected_to root_url
+  end
 end
