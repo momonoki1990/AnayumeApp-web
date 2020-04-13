71f61abaf67d3c3ef3ae5d5ef97cacd29985469f
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-04-13 16:05:49 +0900

    [add]capistranoを実装

--- config/puma.rb
+++ config/puma.rb
@@ -58,7 +58,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 # add setting for production
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path('..', __dir__)
 bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
