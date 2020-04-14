8421d53daa9117892900151a82780e08e44a6480
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-04-14 10:01:06 +0900

    [add]capistranoを実装

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,15 +59,33 @@ set :ssh_options, keepalive: true
 
 # Deployタスク
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'puma:restart'
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  desc "Make sure local git is in sync with remote."
+  task :confirm do
+    on roles(:app) do
+      puts "This stage is '#{fetch(:stage)}'. Deploying branch is '#{fetch(:branch)}'."
+      puts 'Are you sure? [y/n]'
+      ask :answer, 'n'
+      if fetch(:answer) != 'y'
+        puts 'deploy stopped'
+        exit
+      end
     end
   end
+
+  desc "Initial Deploy"
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc "Restart Application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting, :confirm
 end
