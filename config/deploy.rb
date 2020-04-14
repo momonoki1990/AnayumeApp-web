97c7cf5f2d7c0e082cac325220f0ef26d299bb21
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-04-15 08:26:51 +0900

    [add]CDラインの構築

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,19 +59,6 @@ set :ssh_options, keepalive: true
 
 # Deployタスク
 namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :confirm do
-    on roles(:app) do
-      puts "This stage is '#{fetch(:stage)}'. Deploying branch is '#{fetch(:branch)}'."
-      puts 'Are you sure? [y/n]'
-      ask :answer, 'n'
-      if fetch(:answer) != 'y'
-        puts 'deploy stopped'
-        exit
-      end
-    end
-  end
-
   desc "Initial Deploy"
   task :initial do
     on roles(:app) do
