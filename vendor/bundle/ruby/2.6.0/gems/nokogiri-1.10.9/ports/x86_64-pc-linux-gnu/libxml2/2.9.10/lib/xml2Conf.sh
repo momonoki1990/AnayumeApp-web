bdfe739d9da4f7ad3f186ebcb1e1e848e983f99d
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-05-19 06:21:35 +0900

    git rm --chached -r vendor/bundle

--- vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ports/x86_64-pc-linux-gnu/libxml2/2.9.10/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/workdir/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ports/x86_64-pc-linux-gnu/libxml2/2.9.10/lib"
-XML2_LIBS="-lxml2 -lz -llzma    -lm "
-XML2_INCLUDEDIR="-I/workdir/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.9/ports/x86_64-pc-linux-gnu/libxml2/2.9.10/include/libxml2"
-MODULE_VERSION="xml2-2.9.10"
-
