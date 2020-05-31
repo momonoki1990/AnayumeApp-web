ca6009cd5e80969d084a309d0bde0178fe1c561b
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-05-31 20:36:16 +0900

    [fix]コードの改善

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,11 @@
 
 class StaticPagesController < ApplicationController
   def home
-    @dreampost = current_user.dreamposts.build if logged_in?
-
     # ログイン状態によってhome画面で表示する投稿を分ける(ステータスフィードor全投稿)
     if logged_in?
-      @feed_items = current_user.feed.page(params[:page]).per(10)
       @user = current_user
+      @feed_items = @user.feed.page(params[:page]).per(10)
+      @dreampost = @user.dreamposts.build
     else
       @feed_items = Dreampost.all.page(params[:page]).per(10)
     end
